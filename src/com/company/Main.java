c8bf0837cfcd30e0e965270bb730d1e94ff6bb3e
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2019-12-19 20:47:03 +0200

    xczxv

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,14 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-int a , b,c;
-double d;
-a=1;
-b=2;
-c=3;
-d=3.5;
-double rez=a+b+c-d;
+        int a, b, c;
+        double d;
+        a = 1;
+        b = 2;
+        c = 3;
+        d = 3.5;
+        double rez = a + b + c - d;
         System.out.println(rez);
-zscbhzschjbzhjbzxcbhj
-
+        System.out.println(a);
     }
 }
