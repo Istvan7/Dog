8a439a7dcdf2d7736c03e7006fbab423cd6f565d
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2019-12-19 20:39:01 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+int a , b,c;
+double d;
+a=1;
+b=2;
+c=3;
+d=3.5;
+double rez=a+b+c-d;
+        System.out.println(rez);
+
+
+    }
+}
