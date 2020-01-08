4684b358df1fcfbb0888f242f60423c145d46ff1
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2020-01-08 18:28:27 +0200

    xczxv

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,18 @@ public class Main {
     public static void main(String[] args) {
         int a, b, c;
         double d;
-        a = 1;
-        b = 2;
+        a = 5;
+        b = 6;
         c = 3;
-        d = 3.5;
-        double rez = a + b + c - d;
-        System.out.println(rez);
-        System.out.println(a);
+        d = 10;
+       int Prolbema2=a+b;
+       double Problema3=c/d;
+        System.out.println("Hello \n Istvan");
+        System.out.println(Prolbema2);
+        System.out.println(Problema3);
+        System.out.println(-5+8*6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
     }
 }
