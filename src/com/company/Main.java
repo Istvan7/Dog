f0157acbcbdacb18c8e6342065598bd8e382bf40
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2020-01-09 12:28:08 +0200

    xczxv

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,32 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int a, b, c;
+        int a, b, c,e,f,g,h,i,j,k,l;
         double d;
         a = 5;
         b = 6;
         c = 3;
         d = 10;
+        e = 8;
+        f = 55;
+        g = 9;
+        h=-3;
+        i=20;
+        j=3;
+        k=15;
+        l=2;
        int Prolbema2=a+b;
        double Problema3=c/d;
+       int Problema4_1 =(-a+e*b);
+       int Problema4_2 =((f+g)%g);
+       int Problema4_3 =(i+h*a/e);
+       int Problema4_4 =(a+k/j*l-e%j);
         System.out.println("Hello \n Istvan");
         System.out.println(Prolbema2);
         System.out.println(Problema3);
-        System.out.println(-5+8*6);
-        System.out.println((55+9) % 9);
-        System.out.println(20 + -3*5 / 8);
-        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+        System.out.println(Problema4_1);
+        System.out.println(Problema4_2);
+        System.out.println(Problema4_3);
+        System.out.println(Problema4_4);
     }
 }
