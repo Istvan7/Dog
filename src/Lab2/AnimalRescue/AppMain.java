b9a57f6651db8d188c8d8243b0baba543a22925a
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2020-01-15 19:43:34 +0200

    Lab2

--- src/Lab2/AnimalRescue/AppMain.java
+++ src/Lab2/AnimalRescue/AppMain.java
@@ -3,6 +3,6 @@ package Lab2.AnimalRescue;
 public class AppMain {
     public static void main(String[] args) {
         Dog rex = new Dog();
-        rex.
+        System.out.println(rex.age);
     }
 }
