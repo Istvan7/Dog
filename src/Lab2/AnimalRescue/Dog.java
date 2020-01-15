5f1ab5e630a93864473dc833cdb837e8126d2caf
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2020-01-15 19:45:40 +0200

    Lab2

---
+++ src/Lab2/AnimalRescue/Dog.java
@@ -0,0 +1,20 @@
+package Lab2.AnimalRescue;
+
+public class Dog {
+    String name ;
+    String race ;
+    float age ;
+    int size;
+    boolean isFriendly;
+
+    public void run(){
+        System.out.println("(tries to)Runs all day!");
+    }
+    public void sleep (){
+        System.out.println("Snors a lot");
+
+    }
+    public void eat (){
+        System.out.println("Eats 3 meals a day ");
+    }
+}
